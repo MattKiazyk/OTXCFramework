0775dde7cffb3e88f384e9a29fd37629ccff1f5d
Matt Kiazyk <mattkiazyk@gmail.com>
2022-11-16 11:26:41 -0600

    Update url of zip

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: packageName,
-            url: "https://github.com/MattKiazyk/OTXCFramework/blob/main/OTXCFramework.zip",
+            url: "https://github.com/MattKiazyk/OTXCFramework/raw/main/OTXCFramework.zip",
             checksum: "e08bb1189a5bcbac5200583ddc5c430c80da42688eab000cb4086826d73e7a17"
             ),
     ]
