174beb87a6898dd5aa3a06df4a0557a3426e11e9
Matt Kiazyk <mattkiazyk@gmail.com>
2023-03-02 08:58:29 -0600

    Add linkerSettings + wrapper

--- Package.swift
+++ Package.swift
@@ -11,17 +11,38 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: packageName,
-            targets: [packageName]),
+            targets: ["OpenTokResources"]),
     ],
     dependencies: [
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "OpenTokResources",
+            dependencies: [
+                .target(name: packageName)
+            ],
+            linkerSettings: [
+                .linkedFramework("Foundation"),
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("AudioToolbox"),
+                .linkedFramework("CoreFoundation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreTelephony"),
+                .linkedFramework("CoreVideo"),
+                .linkedFramework("GLKit"),
+                .linkedFramework("OpenGLES"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("VideoToolbox")
+            ]
+        ),
         .binaryTarget(
             name: packageName,
             url: "https://github.com/MattKiazyk/OpenTok/raw/main/OpenTok.zip",
             checksum: "4f64d1fbbd7e6f0aef490b4d3ae072057b5a12d814711da6c11892065de9c857"
-            ),
+            
+            )
     ]
 )
